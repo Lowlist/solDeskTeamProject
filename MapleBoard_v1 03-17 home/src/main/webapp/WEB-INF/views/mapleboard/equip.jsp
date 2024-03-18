01759740255d5fa0291d4111aeeee064ece40592
lowlist <dud5825@naver.com>
2024-03-18 14:43:21 +0900

    -14시 42분- 타일런트 15성만 나오고 div css수정 by영민

--- MapleBoard_v1 03-17 home/src/main/webapp/WEB-INF/views/mapleboard/equip.jsp
+++ MapleBoard_v1 03-17 home/src/main/webapp/WEB-INF/views/mapleboard/equip.jsp
@@ -18,19 +18,19 @@
 <body>
 
 <div class="item_popup_main">
-    <div id="starforce" class="star_force_line"></div>
-    <div class="item_top_line">
-        <div id="item_name"></div><br>
-        <div class="item_potential_line" id="item_potential"></div>
+    <div id="starforce_popup" class="starforce_line_popup"></div>
+    <div class="top_line_popup">
+        <div id="item_name_popup"></div><br>
+        <div class="potential_line_popup" id="potential_popup"></div>
     </div>
-    <div class="item_icon_line">
-        <img id="img_icon">
+    <div class="icon_line_popup">
+        <img id="item_icon_popup">
     </div>
 </div>
 
-<div id="main">
-    <div id="eqip">
-        <div id="eqipline" class="item-line">
+<div class="eqip_main">
+    <div class="eqip">
+        <div id="eqipline" class="item_line">
             <c:forEach var="i" begin="0" end="23">
                 <div class="item" id="item_${i}"
                     data-starforce="${item.item_equipment.get(i).starforce}"
@@ -57,15 +57,21 @@
 	                data-total_max_mp_rate="${item.item_equipment.get(i).item_total_option.max_mp_rate}">
                     <img class="itemimg" id="item_${i}_img" src="${item.item_equipment.get(i).item_shape_icon}" alt="${item.item_equipment.get(i).item_name}">
                 </div>
-                <c:if test="${i % 5 == 4}">
-				    <div class="block" id="block_${Math.floor((i / 5)) + 1}"></div>
-				</c:if>
+	                <c:if test="${i % 5 == 3}">
+	                <fmt:parseNumber var="number" value="${Math.floor(i / 5)+ 1}" integerOnly="true"/>
+					    <div class="block" id="block_${number}"></div>
+					</c:if>
             </c:forEach>
-                <fmt:parseNumber var="test" value="${Math.floor((i / 5)) + 1}" integerOnly="true"/>
+            <div class="item" id="item_24"
+            data-name="${android.android_name}"
+            data-potential=""
+            >
+            <img class="itemimg" id="item_24_img" src="${android.android_icon}">
+            </div>
         </div>
     </div>
 
-    <div id="pet">
+    <div class="pet_main">
         <c:forEach var="i" begin="1" end="3">
             <div class="pet-line" id="petline_${i}">
                 <div class="item" id="pet_${i}"></div>
--- MapleBoard_v1 03-17 home/src/main/webapp/resources/maplescript.js
+++ MapleBoard_v1 03-17 home/src/main/webapp/resources/maplescript.js
@@ -2,42 +2,57 @@ $(document).ready(function() {
     $('.item').hover(function() {
         let starForceData = $(this).data('starforce');
         let equip_level = $(this).data('equip_level');
-        starForceRun(starForceData, equip_level);
         let dataName = String($(this).data('name'));
         let dataPotential = String($(this).data('potential'));
         let itemIcon = $(this).find('img').attr('src');
-        $('#item_name').text(dataName);
+        starForceRun(starForceData, equip_level,dataName);
+        $('#item_name_popup').text(dataName);
         //? 을 사용해서 있으면 true 없으면 false 반환
-        $('#item_potential').text(dataPotential ? "(" + dataPotential + " 아이템)" : "");
-        $('#img_icon').attr('src', itemIcon).attr('class', dataPotential);
+        $('#potential_popup').text(dataPotential ? "(" + dataPotential + " 아이템)" : "");
+        $('#item_icon_popup').attr('src', itemIcon).attr('class', dataPotential);
         $('.item_popup_main').show();
-        $('.star_force_line').show();
+        $('.starforce_line_popup').show();
     }, function() {
         $('.item_popup_main').hide();
-        $('.star_force_line').hide();
+        $('.starforce_line_popup').hide();
     });
 });
 
-function starForceRun(starforce, equipLevel) {
+function starForceRun(starforce, equipLevel,dataName) {
     let maxStarForce;
     let starimg = "https://cdn.dak.gg/maple/images/tooltip/item/ico-starforce-yellow.png";
     let starblackimg = "https://cdn.dak.gg/maple/images/tooltip/item/ico-starforce-default.png";
-    let container = $("#starforce");
+    let container = $("#starforce_popup");
     container.empty();
     let equipLevelCheck = parseInt(equipLevel);
-    
-    if (equipLevelCheck <= 94) {
-        maxStarForce = 5;
-    } else if (equipLevelCheck <= 107) {
-        maxStarForce = 7;
-    } else if (equipLevelCheck <= 117) {
-        maxStarForce = 10;
-    } else if (equipLevelCheck <= 127) {
-        maxStarForce = 15;
-    } else if (equipLevelCheck <= 137) {
-        maxStarForce = 20;
-    } else if (equipLevelCheck >= 138) {
-        maxStarForce = 25;
+    if(dataName.includes("타일런트") || dataName.includes("노바")||dataName.includes("헬리시움")){
+        if (equipLevelCheck <= 94) {
+            maxStarForce = 3;
+        } else if (equipLevelCheck <= 107) {
+            maxStarForce = 5;
+        } else if (equipLevelCheck <= 117) {
+            maxStarForce = 8;
+        } else if (equipLevelCheck <= 127) {
+            maxStarForce = 10;
+        } else if (equipLevelCheck <= 137) {
+            maxStarForce = 12;
+        } else if (equipLevelCheck >= 138) {
+            maxStarForce = 15;
+        }
+    } else {
+        if (equipLevelCheck <= 94) {
+            maxStarForce = 5;
+        } else if (equipLevelCheck <= 107) {
+            maxStarForce = 7;
+        } else if (equipLevelCheck <= 117) {
+            maxStarForce = 10;
+        } else if (equipLevelCheck <= 127) {
+            maxStarForce = 15;
+        } else if (equipLevelCheck <= 137) {
+            maxStarForce = 20;
+        } else if (equipLevelCheck >= 138) {
+            maxStarForce = 25;
+        }
     }
 
     if (starforce != 0) {
